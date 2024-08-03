bdaa0df579dbdaf36e91e902a4ad80f463f5285d
cuongdev04 <cuongnvph36558@fpt.edu.vn>
2024-08-03 08:24:55 +0700

    update_lai_03/08

--- controller/index.php
+++ controller/index.php
@@ -528,10 +528,10 @@ if(isset($_GET['act'])&&($_GET['act']!="")){
                                 }
                                 if ($check) {
                                     insert_cart($_SESSION['user']['id'], $sanpham['id'], $soluong, $thanhtien);
-                                    header("location: ?act=giohang");
+                                    header("location: ?act=tieptucdathang");
                                 }
                         }else{
-                            header("location: ?act=tieptucdathang");
+                            header("location: ?act=dangnhap");
                         }
                     }
                 }
